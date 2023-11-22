ed24844f00828667d6b2d868b78658e2556df5d5
yjlsxzwbs <3151955109@qq.com>
2023-11-22 21:19:38 +0800

    事件循环机制

---
+++ js/event-loop/async.js
@@ -0,0 +1,31 @@
+function A(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('异步A完成');
+            resolve()
+        },1000)
+    })
+}
+function B(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('异步B完成');
+            resolve()
+        },500)
+    })
+}
+function C(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('异步C完成');
+            resolve()
+        },100)
+    })
+}
+
+async function foo(){ 
+    await A()//await 会阻塞后续代码的执行，将后续代码推入到微任务队列
+    console.log(1);
+    await B()
+    await C()
+}
