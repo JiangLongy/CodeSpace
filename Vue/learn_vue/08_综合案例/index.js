edc7ec95744cb5b88e53de8d8fcd648c774a24ec
yjlsxzwbs <3151955109@qq.com>
2023-11-29 21:37:46 +0800

    Vue

---
+++ Vue/learn_vue/08_综合案例/index.js
@@ -0,0 +1,57 @@
+Vue.createApp({
+    template: "#my-app",
+    data() {
+      return {
+        books: [
+          {
+            id: 1,
+            name: '《算法导论》',
+            date: '2006-9',
+            price: 85.00,
+            count: 1
+          },
+          {
+            id: 2,
+            name: '《UNIX编程艺术》',
+            date: '2006-2',
+            price: 59.00,
+            count: 1
+          },
+          {
+            id: 3,
+            name: '《编程珠玑》',
+            date: '2008-10',
+            price: 39.00,
+            count: 1
+          },
+          {
+            id: 4,
+            name: '《代码大全》',
+            date: '2006-3',
+            price: 128.00,
+            count: 1
+          },          
+        ],
+      }
+    },
+    computed: {
+        allPrice(){
+            let total = 0
+            for(let item of this.books) {
+                total += item.price * item.count
+            }
+            return total
+        }
+    },
+    methods: {
+      del(i){
+        this.books.splice(i,1)
+      },
+      add(i){
+        this.books[i].count++
+      },
+      jian(i){
+        if(this.books[i].count>1)this.books[i].count--
+      }
+    }
+  }).mount("#app");
