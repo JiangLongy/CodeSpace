f9a9466b919c2193f52a105e6cc334f3b92690e9
yjlsxzwbs <3151955109@qq.com>
2023-12-04 21:43:19 +0800

    vite

---
+++ Vue/vue-router/src/views/homeChild/newest.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ol>
+            <li>推荐2</li>
+            <li>推荐3</li>
+            <li>推荐4</li>
+            <li>推荐5</li>
+        </ol>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ Vue/vue-router/src/views/homeChild/recommend.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ul>
+            <li>推荐1</li>
+            <li>推荐1</li>
+            <li>推荐1</li>
+            <li>推荐1</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
