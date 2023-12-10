820eb5821f87f9ece134531c614b539f36e0c2a5
yjlsxzwbs <3151955109@qq.com>
2023-12-10 22:58:48 +0800

    手写bind,params

---
+++ js/myBind/1.js
@@ -0,0 +1,29 @@
+Function.prototype.myBind = function(context,...args){
+    //函数 this
+    // this()
+    if(typeof this!== "function"){
+        throw new TypeError("error")
+    }
+    context = context || window//闭包之中
+    let that = this;
+    return function fn(...innerArgs){
+        //this丢失
+        if(this instanceof fn){
+            return new that(...args,...innerArgs)
+        }
+        return that.apply(context,[...args,...innerArgs])
+        //原先的函数执行，this 手动指定为context
+    }
+}
+function sayHello(x, y, z) {
+    console.log("函数中的",this);
+    console.log("你好！",this.name);
+    console.log(x + y + z);
+}
+const arrowFn = ()=>{}
+let person = {
+    name:"坤坤"
+}
+// arrowFn.myBind()
+let bindFn = sayHello.myBind(person,1,2,);
+console.log(bindFn(3));
---
+++ js/myBind/readme.md
@@ -0,0 +1,11 @@
+# 手写bind
+- 扩展Function的bind myBind 
+- 绑定函数的this的上下文
+- 收集参数，bind时 ...args
+    执行时 ...args1
+    [...args,...args1]
+- 闭包，返回函数
+- 
+
+- 何时用bind
+    当事件处理函数在对象内部时，
