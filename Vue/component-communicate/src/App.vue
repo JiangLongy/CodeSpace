983d8d0e423c38ddd90e673302eb36551762703c
JiangLongy <3151955109@qq.com>
2023-12-18 21:43:44 +0800

    组件通信

---
+++ Vue/component-communicate/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="head">
+      <input type="text" name="" v-model="message">
+      <button @click="submit">确定</button>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        lists:['html','css'],
+        message:''
+      }
+    },
+    methods:{
+      submit(){
+        if(!this.message) return 
+        this.lists.push(this.message)
+        this.message=''
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ Vue/component-communicate/src/components/body4/Head.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="head">
+        <input type="text" name="" v-model="message">
+        <button @click="submit">确定</button>
+      </div>
+    </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+    export default {
+        data(){
+        return {     
+          message:''
+        }
+      },
+      methods:{
+        submit(){
+
+         this.listsAdd(this.message)
+
+        },
+        ...mapMutations(['listsAdd'])
+        
+      }
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
