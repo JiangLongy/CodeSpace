836777f8598683e0c7a70e18582e4250defedb39
JiangLongy <3151955109@qq.com>
2023-12-22 21:34:42 +0800

    pinia

---
+++ Vue/pinia-demos/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <TodoList :todos="todoStore.todos" 
+              :addTodo="todoStore.addTodo"/>
+    <Item />
+    <Bang :title="作者榜" :item="authors" />
+  </div>
+</template>
+
+<script setup>
+// 以组件为开发单元
+import TodoList from './components/TodoList.vue'
+import {ref} from 'vue'
+// const todos = ref([
+//   {text:'haha'}
+// ])
+//todos 来到 vuex mapState pinia
+import {useTodoStore} from './store/todos.js'
+const {todos,addTodo} = useTodoStore()
+const todoStore = useTodoStore()
+// console.log(todoStore.todos);
+</script>
+
+<style scoped>
+
+</style>
