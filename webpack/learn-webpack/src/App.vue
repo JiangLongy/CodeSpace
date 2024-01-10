87d69537fc93a65d50b229554da08560f9013f3c
JiangLongy <3151955109@qq.com>
2024-01-10 19:11:58 +0800

    webpack

---
+++ webpack/learn-webpack/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="wrap">
+        helloworld!
+    </div>
+</template>
+
+<script>
+    export default {      
+    }
+</script>
+
+<style lang="css" scoped>
+.wrap{
+    width: 300px;
+    height: 300px;
+    background: #000;
+    color: #fff;
+    font-size: 20px;
+}
+</style>
