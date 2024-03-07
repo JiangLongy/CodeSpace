a0c40355db1f3087a8fa00152ee1f4fd46c9b869
JiangLong <3151955109@qq.com>
2024-03-07 22:27:41 +0800

    百度面试题

---
+++ 面试题/js系列/百度面试题/2.js
@@ -0,0 +1,36 @@
+//red 3s
+//yellow 2s
+//green 1s
+
+function red(){
+    console.log('红');
+}
+function yellow(){
+    console.log('黄');
+}
+function green(){
+    console.log('绿');
+}
+
+function light(cb,wait){
+   return new Promise((resolve)=>{
+       setTimeout(()=>{
+           cb();
+           resolve();
+       },wait)
+   })
+}
+
+function lightStep(){
+    Promise.resolve().then(()=>{
+        return light(red,3000);
+    }).then(()=>{
+        return light(yellow,2000);
+    })
+    .then(()=>{
+        return light(green,1000);
+    }).finally(()=>{
+       return lightStep() 
+    })
+}
+lightStep()
